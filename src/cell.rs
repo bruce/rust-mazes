47e054ac7d8a9b45569f596f6d039a3b8a5e841b
Bruce Williams <brwcodes@gmail.com>
2015-04-26 21:33:03 -0700

    Restructure for HashMap
    (Does not yet compile.)

--- src/cell.rs
+++ src/cell.rs
@@ -1,23 +1,18 @@
 use std::rc::Rc;
 use std::default::Default;
 
+pub type CellIndex = (usize, usize); // col, row
+
 #[derive(Debug)]
 pub struct Cell {
-    pub column: usize,
-    pub row: usize,
-    pub placement: Placement,
-    pub north: Option<Rc<Cell>>,
-    pub south: Option<Rc<Cell>>,
-    pub east: Option<Rc<Cell>>,
-    pub west: Option<Rc<Cell>>
+    pub index: CellIndex,
+    pub placement: Placement
 }
 
 impl Default for Cell {
     fn default() -> Cell {
-        Cell { column: 0, row: 0,
-               placement: Placement::Normal,
-               north: None, south: None,
-               east: None, west: None }
+        Cell { index: (0, 0),
+               placement: Placement::Normal }
     }
 }
 
--- src/grid.rs
+++ src/grid.rs
@@ -1,4 +1,5 @@
 use std::default::Default;
+use std::collections::HashMap;
 
 use cell::*;
 
@@ -6,12 +7,11 @@ use cell::*;
 pub struct Grid {
     pub width: usize,
     pub height: usize,
-    // Each internal Vec is a column of cells
-    pub cells: Vec<Vec<Cell>>
+    pub cells: HashMap<CellIndex, Cell>
 }
 impl Default for Grid {
     fn default() -> Grid {
-        Grid { width: 0, height: 0, cells: vec![] }
+        Grid { width: 0, height: 0, cells: HashMap::new() }
     }
 }
 
@@ -20,18 +20,12 @@ impl Grid {
     pub fn new(cols: usize, rows: usize) -> Grid {
         let mut g = Grid { width: cols, height: rows, .. Default::default() };
         for col in (1..cols) {
-            let mut col_vec = vec![];
             for row in (1..rows) {
-                let cell = Cell {column: col, row: row, .. Default::default() };
-                col_vec.push(cell);
+                g.cells.insert((col, row),
+                               Cell {index: (col, row), .. Default::default() })
             }
-            g.cells.push(col_vec);
         }
         g
-    }
-
-    pub fn cell(&self, column: usize, row: usize) -> &Cell {
-        &self.cells[column - 1][row - 1]
     }
 
 }
